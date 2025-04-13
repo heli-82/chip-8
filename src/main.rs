ce9c2096d9a681e6faebef16398da909af1480c4
heli82 <89199260267den@gmail.com>
2025-04-13 16:57:13 +0500

    Games!

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ impl Chip8 {
 }
 
 fn main() {
-    let mut file = File::open("roms/programs/IBM Logo.ch8").unwrap();
+    let mut file = File::open("chip8-roms/programs/IBM Logo.ch8").unwrap();
     let mut data = Vec::<u8>::new();
     file.read_to_end(&mut data).unwrap();
 
